fa2e74bcfc2d6e8518d98b13827170fc4da27a3d
7h0mas-R <thomas.rohleder@gmx.net>
2022-04-28 21:36:57 +0200

    largely working

--- eadog_lcd/index.js
+++ eadog_lcd/index.js
@@ -8,6 +8,7 @@ var execSync = require('child_process').execSync;
 var font = require('../../LCD/font');
 var lcd = require('../../LCD/eadog-spi-lcd')
 var io = require('socket.io-client');
+const { start } = require('repl');
 const fontStyles = require('../../LCD/font').fontStyle
 const animationTypes = require('../../LCD/eadog-spi-lcd').animationTypes
 
@@ -15,6 +16,9 @@ const states = Object.freeze({
     starting: 0,
     status: 1,
     menu: 3,
+    error: 4,
+    info: 5,
+    critical: 6,
     stopping: 10
 })
 
@@ -53,15 +57,15 @@ eadogLcd.prototype.onStart = function() {
     self.debugLogging = (self.config.get('logging')==true);
 	if (self.debugLogging) self.logger.info('[EADOG_LCD] onStart: starting plugin');
     self.socket = io.connect('http://volumio:3000');
-    self.activateListeners();
 
     //############################### improve, path is not good
     self.font_prop_16px.loadFontFromJSON('./LCD/Fonts_JSON/font_proportional_16px.json');
     self.font_prop_8px.loadFontFromJSON('./LCD/Fonts_JSON/font_proportional_8px.json');
     self.font_prop_16px.spacing = 0;
-    self.state = {};
+    self.state = 0;
     self.activePage = 0;
     self.selectedLine = 0;
+    self.currentLevel = self.config.get('startLevel');
     self.display.initialize({pinCd: 25, pinRst: 20, speedHz: 800000, viewDirection: 0, volume: 6})
     .then(_ => self.display.clear())
     .then(_ => self.display.setPageBufferLines(0,"UNDA 3.0",self.font_prop_16px))
@@ -69,9 +73,19 @@ eadogLcd.prototype.onStart = function() {
     .then(_ => self.display.setPageBufferLines(4,"       Volumio 3",self.font_prop_8px,0,animationTypes.swingPage))
     .then(_ => self.display.setPageBufferLines(7,"(C)2022 7h0mas-R",self.font_prop_8px,0,animationTypes.swingPage))
     .then(_ => self.display.startAnimation(1000))
+    .then(_ => {
+        let timeout = parseInt(self.config.get('splashScreenTimeout'));
+        if (self.debugLogging) this.logger.info('[EADOG_LCD] onStart: setting SplashScreenTimer to ' + timeout + ' ms.')
+        setTimeout(() => {
+            if (self.debugLogging) this.logger.info('[EADOG_LCD] onStart: SplashScreenTimer elapsed')
+            self.display.clear()
+            .then(_=>self.activateListeners());
+        }, timeout)
+    })    
+
 	// Once the Plugin has successfull started resolve the promise
     .then(_ => defer.resolve())
-    .catch(err => self.logger.error('[EADOG_LCD] onStart: failed with ',err))
+    .catch(err => self.logger.error('[EADOG_LCD] onStart: failed with ', err))
 
     return defer.promise;
 };
@@ -168,12 +182,17 @@ eadogLcd.prototype.activateListeners = function () {
         if (self.debugLogging) self.logger.info('[EADOG LCD] Push: ' + state.status + " - " + state.artist +  " - " + state.title);
         self.updateStatus(state);
     });
-    self.socket.emit('getBrowseSources');
+    let startUri = self.config.get('startLevel');
+    if (startUri != undefined && startUri!="/") {
+        self.socket.emit('browseLibrary',{"uri":startUri});    
+    } else {
+        self.socket.emit('getBrowseSources');
+    }
     self.socket.emit('getState');
     // self.socket.emit('browseLibrary',{"uri":"radio/selection"});
 }
 
-eadogLcd.prototype.deactivateListeners = function (params) {
+eadogLcd.prototype.deactivateListeners = function () {
     var self = this;
     self.socket.off();
     // self.socket.off('pushBrowseSources');
@@ -183,146 +202,252 @@ eadogLcd.prototype.deactivateListeners = function (params) {
 
 eadogLcd.prototype.up = function(){
     var self = this;
-    if (self.menuItems != undefined) {
-        self.selectedItem = (self.activePage * self.maxLine + self.selectedLine)
-        self.selectedItem = (self.selectedItem + self.menuItems.length - 1) % self.menuItems.length;
-        self.activePage = Math.floor(self.selectedItem/self.maxLine);
-        self.selectedLine = self.selectedItem % self.maxLine;
-        self.refreshDisplay(self.menuItems)
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] up: Received up command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            if (self.menuItems != undefined) {
+                self.selectedItem = (self.activePage * self.maxLine + self.selectedLine)
+                self.selectedItem = (self.selectedItem + self.menuItems.length - 1) % self.menuItems.length;
+                self.activePage = Math.floor(self.selectedItem/self.maxLine);
+                self.selectedLine = self.selectedItem % self.maxLine;
+                self.refreshDisplay(self.menuItems)
+            }            
+            break;
+    
+        default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
+            break;
     }
+    self.resetMenuTimer();
 }
 
 eadogLcd.prototype.down = function(){
     var self = this;
-    if (self.menuItems != undefined) {
-        self.selectedItem = (self.activePage * self.maxLine + self.selectedLine)
-        self.selectedItem = (self.selectedItem + 1) % self.menuItems.length;
-        self.activePage = Math.floor(self.selectedItem/self.maxLine);
-        self.selectedLine = self.selectedItem % self.maxLine;
-        self.refreshDisplay(self.menuItems)
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] down: Received down command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            if (self.menuItems != undefined) {
+                self.selectedItem = (self.activePage * self.maxLine + self.selectedLine)
+                self.selectedItem = (self.selectedItem + 1) % self.menuItems.length;
+                self.activePage = Math.floor(self.selectedItem/self.maxLine);
+                self.selectedLine = self.selectedItem % self.maxLine;
+                self.refreshDisplay(self.menuItems)
+            }
+            break;
+    
+        default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
+            break;
     }
+    self.resetMenuTimer();
 }
 
  eadogLcd.prototype.select = function(){
     var self = this;
-    let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
-    let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
-    switch (selectedType) {
-        case 'music_service':
-        case 'radio-category':
-        case 'folder':
-            self.socket.emit('browseLibrary',{"uri":self.menuItems[selectedItem].uri})
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] select: Received select command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
+            let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
+            switch (selectedType) {
+                case 'music_service':
+                case 'radio-category':
+                case 'radio-favourites':
+                case 'folder':
+                    self.currentLevel = self.menuItems[selectedItem].uri
+                    self.socket.emit('browseLibrary',{"uri":self.currentLevel})
+                    break;
+                case 'webradio':
+                    self.socket.emit('replaceAndPlay',self.menuItems[selectedItem])
+                case 'song':
+                case 'playlist':
+                    self.socket.emit('addPlay',self.menuItems[selectedItem])
+                    break;
+                default:
+                    break;
+            }
             break;
-        case 'webradio':
-            self.socket.emit('replaceAndPlay',self.menuItems[selectedItem])
-        case 'song':
-        case 'playlist':
-            self.socket.emit('addPlay',self.menuItems[selectedItem])
-            break;
+    
         default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
             break;
     }
+    self.resetMenuTimer();
 }
 
  eadogLcd.prototype.addToQueueAndPlay = function(){
     var self = this;
-    let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
-    let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
-    switch (selectedType) {
-        case 'music_service':
-        case 'radio-category':
-        case 'folder':
-        case 'song':
-        case 'playlist':
-            self.socket.emit('addToQueue',{"uri":self.menuItems[selectedItem].uri});
-            self.socket.emit('play')
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] addToQueueAndPlay: Received command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
+            let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
+            switch (selectedType) {
+                case 'music_service':
+                case 'radio-category':
+                case 'folder':
+                case 'song':
+                case 'playlist':
+                    self.socket.emit('addToQueue',{"uri":self.menuItems[selectedItem].uri});
+                    self.socket.emit('play')
+                    break;
+                case 'webradio':
+                    break;
+                default:
+                    break;
+            }
             break;
-        case 'webradio':
-            break;
+    
         default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
             break;
     }
+    self.resetMenuTimer();
 }
 
  eadogLcd.prototype.replaceQueueAndPlay = function(){
     var self = this;
-    let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
-    let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
-    switch (selectedType) {
-        case 'music_service':
-        case 'radio-category':
-        case 'folder':
-        case 'song':
-        case 'playlist':
-            self.socket.emit('clearQueue');
-            self.socket.emit('addToQueue',{"uri":self.menuItems[selectedItem].uri});
-            setTimeout(() => {
-                self.socket.emit('play');
-            }, 150); 
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] replaceQueueAndPlay: Received command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            let selectedItem = (self.activePage * self.maxLine + self.selectedLine)
+            let selectedType = self.menuItems[selectedItem].plugin_type != undefined? self.menuItems[selectedItem].plugin_type: self.menuItems[selectedItem].type
+            switch (selectedType) {
+                case 'music_service':
+                case 'radio-category':
+                case 'folder':
+                case 'song':
+                case 'playlist':
+                    self.socket.emit('clearQueue');
+                    self.socket.emit('addToQueue',{"uri":self.menuItems[selectedItem].uri});
+                    setTimeout(() => {
+                        self.socket.emit('play');
+                    }, 150); 
+                    break;
+                case 'webradio':
+                    break;
+                default:
+                    break;
+            }
             break;
-        case 'webradio':
-            break;
+    
         default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
             break;
     }
+    self.resetMenuTimer();
 }
  eadogLcd.prototype.back = function(){
     var self = this;
-    if (self.previousLevel != undefined) {
-        if (self.previousLevel == "/") {
-            self.socket.emit('getBrowseSources');
-        } else {
-            self.socket.emit('browseLibrary',{"uri":self.previousLevel})
-        }
-    }
-}
-eadogLcd.prototype.updateStatus = function (state){
-    var self = this;
-
-    if (self.state.status == undefined || state.status != self.state.status) {
-        self.display.setPageBufferLines(6,self.state.status,self.font_prop_16px,fontStyles.normal,animationTypes.none);
-    }
-    if (self.state.artist == undefined || state.artist != self.state.artist) {
-        self.display.setPageBufferLines(0,self.state.artist,self.font_prop_16px,fontStyles.normal,animationTypes.swingPage);
-    }
-    if (self.state.title == undefined || state.title != self.state.title) {
-        self.display.setPageBufferLines(0,self.state.title,self.font_prop_16px,fontStyles.normal,animationTypes.swingPage);
-    }
-    self.state.status = state.status;
-}
-
-eadogLcd.prototype.refreshDisplay = async function (menuArray){
-    var self = this;
-    let pagesPerLine = self.font_prop_16px._heightBytes;
-    var style = 0;
-    if (menuArray!=undefined) {
-        for (let i = 0; i < self.maxLine; i++) {
-            if (i==self.selectedLine) {
-                style = fontStyles.inverted; //inverted
-            } else {
-                style = fontStyles.normal;  //normal
-            }
-            let item = i+ self.activePage * self.maxLine;
-            let outputLine = "                   ";
-            if (item < menuArray.length) { 
-                switch (menuArray[item].type) {
-                    case 'radio-category':
-                    case 'mywebradio-category':
-                    case 'radio-favourites':
-                    case 'folder':
-                    case 'webradio':
-                    case 'playlist':
-                        outputLine = menuArray[item].title+ "  ";                    
-                        break;
-                    case 'song':
-                        outputLine = menuArray[item].tracknumber + ") " + menuArray[i+ self.activePage * self.maxLine].title + "  ";                    
-                        break;
-                    default:
-                        outputLine = menuArray[item].name;
-                        break;
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] back: Received command in status:' + self.state)
+    switch (self.state) {
+        case states.menu:
+            if (self.previousLevel != undefined) {
+                if (self.previousLevel == "/") {
+                    self.currentLevel = "/";
+                    self.socket.emit('getBrowseSources');
+                } else {
+                    if (self.currentLevel != self.config.get('highestLevel')) {
+                        self.currentLevel = self.previousLevel;
+                    }
+                    self.socket.emit('browseLibrary',{"uri":self.currentLevel})
                 }
             }
-            this.display.setPageBufferLines(i* pagesPerLine,outputLine,self.font_prop_16px,style,animationTypes.rotateStep,51);
-        }
+            break;
+    
+        default:
+            self.state = states.menu;
+            self.refreshDisplay(self.menuItems)
+            break;
+    }
+    self.resetMenuTimer();
+}
+
+eadogLcd.prototype.resetMenuTimer = function () {
+    var self = this;
+    if (self.menuTimer == undefined) {
+        self.menuTimeout = self.config.get('menuTimeout')*1000;
+        if (self.debugLogging) this.logger.info('[EADOG_LCD] resetMenuTimer: creating new Timer')
+        self.menuTimer = setTimeout(() => {
+            if (self.debugLogging) this.logger.info('[EADOG_LCD] resetMenuTimer: menu Timeout elapsed on Timer: ' + self.menuTimer)
+            clearTimeout(self.menuTimer);
+            self.menuTimer = null;
+            self.state = states.status;
+            self.refreshDisplay(self.menuItems);
+        }, self.menuTimeout);
+    } else {
+        if (self.debugLogging) this.logger.info('[EADOG_LCD] resetMenuTimer: restarting Timer: ' + self.menuTimer)
+        self.menuTimer.refresh();
+    }
+}
+eadogLcd.prototype.updateStatus = function (status){
+    var self = this;
+    var wasUndef = false;
+    if (self.debugLogging) this.logger.info('[EADOG_LCD] updateStatus: ' + status);
+    if (status == undefined) {
+        status = self.status;  
+        wasUndef = true ; 
+    } 
+    if (self.state.artist == undefined || state.artist != self.state.artist ) {
+        self.display.setPageBufferLines(0,status.artist + '   ',self.font_prop_16px,fontStyles.normal,animationTypes.rotatePage);
+    }
+    if (self.state.title == undefined || state.title != self.state.title) {
+        self.display.setPageBufferLines(2,status.title + '   ',self.font_prop_16px,fontStyles.normal,animationTypes.rotatePage);
+    }
+    if (wasUndef) {
+        self.display.setPageBufferLines(4," ",self.font_prop_16px,fontStyles.normal,animationTypes.none);
+    }
+    if (self.state.status == undefined || state.status != self.state.status || status == undefined) {
+        self.display.setPageBufferLines(6,status.status,self.font_prop_16px,fontStyles.normal,animationTypes.none);
+    }
+    self.status = status;    
+}
+
+eadogLcd.prototype.refreshDisplay = async function (){
+    var self = this;
+    switch (self.state) {
+        case states.menu:
+            let pagesPerLine = self.font_prop_16px._heightBytes;
+            var style = 0;
+            if (self.menuItems!=undefined) {
+                for (let i = 0; i < self.maxLine; i++) {
+                    if (i==self.selectedLine) {
+                        style = fontStyles.inverted; //inverted
+                    } else {
+                        style = fontStyles.normal;  //normal
+                    }
+                    let item = i+ self.activePage * self.maxLine;
+                    let outputLine = "                   ";
+                    if (item < self.menuItems.length) { 
+                        switch (self.menuItems[item].type) {
+                            case 'radio-category':
+                            case 'mywebradio-category':
+                            case 'radio-favourites':
+                            case 'folder':
+                            case 'webradio':
+                            case 'playlist':
+                                outputLine = self.menuItems[item].title+ "  ";                    
+                                break;
+                            case 'song':
+                                outputLine = self.menuItems[item].tracknumber + ") " + self.menuItems[i+ self.activePage * self.maxLine].title + "  ";                    
+                                break;
+                            default:
+                                outputLine = self.menuItems[item].name;
+                                break;
+                        }
+                    }
+                    this.display.setPageBufferLines(i* pagesPerLine,outputLine,self.font_prop_16px,style,animationTypes.rotateStep,51);
+                }
+            }
+            break;
+    
+        default:
+            this.updateStatus();
+            break;
     }
 }
